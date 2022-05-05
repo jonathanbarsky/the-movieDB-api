b50e7eedb12218a34a2309a570d3c65b1e3c1623
Jonathan Barsky <barskyjonathan96@gmail.com>
2022-05-05 11:27:06 -0300

    se implemento la migracion de fetch a axios en el llamado a la API

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
 
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
-    const data = await res.json();
-
+    const { data } = await api(`trending/movie/day`);
     const movies = data.results;
+    
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview_movieList');
 
@@ -23,10 +30,9 @@ async function getTrendingMoviesPreview() {
 } 
 
 async function getCategoriesPreview() {
-    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const { data } = await api(`genre/movie/list`);
     const categories = data.genres;
+
     categories.forEach(category => {
         const PreviewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview_list');
         const categoryContainer = document.createElement('div')
